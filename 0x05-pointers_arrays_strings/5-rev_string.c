60384b99816294f0296ec9851fe1709ae0c748d0
Peace Bello <pgideon2000@gmail.com>
2022-09-20 04:19:06 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,13 +10,24 @@
 
 void rev_string(char *s)
 {
-	int i = 0, length;
+	int a, len;
 
-	length = _strlen(s) - 1;
-	while (length > i)
+	char *begin, *end = s;
+
+	for (a = 0; s[a] != '\0' && s[a + 1] != '\0'; a++)
 	{
-		swap_char(s + length, s + i);
-		i++;
-		length--;
+		end++;
 	}
+	len = a + 1;
+	begin = s;
+	for (a = 0; a < len / 2; a++)
+	{
+		char x;
+		x = *end;
+		*end = *begin;
+		*begin = x;
+		begin++;
+		end--;
+	}
+	end[len + 1] = '\0';
 }
